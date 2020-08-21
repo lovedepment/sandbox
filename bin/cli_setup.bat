1aa51eee3fa72a90b898a84d521b62249ae67cd3
Herman Gabor <herman.gabor.baja@gmail.com>
2020-08-21 19:27:19 +0200

    Throntail helyett wildfly-on fut.

--- bin/cli_setup.bat
+++ bin/cli_setup.bat
@@ -1,7 +1,9 @@
 @echo off
 
-set JAVA_HOME=C:\Programs\jdk11
+set JAVA_HOME=C:\Programs\jdk14
 set MAVEN_HOME=C:\Programs\maven
+set WILDFLY_HOME=C:\Programs\wildfly
 
-set PATH=%PATH%;%JAVA_HOME%\bin;%MAVEN_HOME%\bin
+set PATH=%PATH%;%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%WILDFLY_HOME%\bin
+
 
--- jaxws-bottomup/server/src/main/java/org/ghermann/learning/jaxwsbottomup/server/GreetingWebService.java
+++ jaxws-bottomup/server/src/main/java/org/ghermann/learning/jaxwsbottomup/server/GreetingWebService.java
@@ -5,27 +5,30 @@ import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.ParameterStyle;
 import javax.jws.soap.SOAPBinding.Style;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebService(name = "GreetingServiceInterface",
             serviceName="GreetingService", portName = "GreetingServicePort")
-@SOAPBinding(style = Style.DOCUMENT, parameterStyle = ParameterStyle.BARE)
+@SOAPBinding(style = Style.DOCUMENT, parameterStyle = SOAPBinding.ParameterStyle.BARE)
 public class GreetingWebService {
     private static final int NAME_LENGTH_MAX = 100;
 
-    @WebResult(name="greeting")
+    @WebResult(name="greeting") @XmlElement(required = true)
+    @WebMethod(operationName = "greetSimply")
     public String greet(
-        @WebParam(name = "firstName") String firstName,
-        @WebParam(name = "lastName") String lastName
+        @XmlElement(required = true) @WebParam(name = "firstName") String firstName,
+        @XmlElement(required = true) @WebParam(name = "lastName") String lastName
     ) {
         return "Hello " + firstName + " " + lastName + "!";
     }
 
-    @WebResult(name="greeting")
+    @WebResult(name="greeting") @XmlElement(required = true)
     public String greetVerbosely(
-        @WebParam(name = "firstName") String firstName,
-        @WebParam(name = "lastName") String lastName
+        @XmlElement(required = true) @WebParam(name = "firstName") String firstName,
+        @XmlElement(required = true) @WebParam(name = "lastName") String lastName
     ) throws BadNameException {
         if (firstName.length() > NAME_LENGTH_MAX) {
             throw new BadNameException(firstName);
@@ -37,18 +40,27 @@ public class GreetingWebService {
         return "Hello " + firstName + " " + lastName + ", How do you do!";
     }
 
-    @WebResult(name="greeting")
-    @WebMethod(operationName = "greetPersonUsingTitle")
+    @WebResult(name="greeting") @XmlElement(required = true)
     public String greetWithTitle(
-        @WebParam(name = "title") Title title,
-        @WebParam(name = "firstName") String firstName,
-        @WebParam(name = "lastName") String lastName
+        @XmlElement(required = true) @WebParam(name = "title") Title title,
+        @XmlElement(required = true) @WebParam(name = "firstName") String firstName,
+        @XmlElement(required = true) @WebParam(name = "lastName") String lastName
     ) {
         return "Hello " + title + " " + firstName + " " + lastName + "!";
     }
 
-    @WebResult(name = "greeting")
-    public String greetPerson(@WebParam(name = "person") Person person) {
+    @WebResult(name = "greeting") @XmlElement(required = true)
+    public String greetPerson(@XmlElement(required = true) @WebParam(name = "person") Person person) {
         return greetWithTitle(person.getTitle(), person.getFirstName(), person.getLastName());
+    }
+
+    @WebResult(name = "greetings") @XmlElement(required = true)
+    public List<String> massGreet(@XmlElement(required = true) @WebParam(name = "persons") Person[] persons) {
+        List<String> result = new ArrayList<>();
+        for (var person : persons) {
+            String greeting = greetWithTitle(person.getTitle(), person.getFirstName(), person.getLastName());
+            result.add(greeting);
+        }
+        return result;
     }
 }
